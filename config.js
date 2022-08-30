b22d8aae20a58524886726503ed004d5140a7635
Fede Souto <federicogsouto@gmail.com>
2022-08-29 21:26:41 -0300

    arregle config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const firebaseServiceAccount = require("./coder-backend-5ae09-firebase-adminsdk-6rlzw-e7ab343996.json");
+
 const mongodbUri = process.env.MONGODB_URI;
 const mongodbUriSessions = process.env.MONGODB_URI_SESSIONS;
 const sessionSecret = process.env.SESSION_SECRET;
